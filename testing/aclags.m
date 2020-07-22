8f2676e119d639e8582d0b1c9d8a373daf555a87
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2020-07-22 17:28:35 +0100

    removed all code from testing/

--- testing/aclags.m
+++
@@ -1,5 +0,0 @@
-function q = aclags(rho,acdectol)
-
-if nargin < 2 || isempty(acdectol), acdectol  = 1e-8; end % autocovariance decay tolerance
-
-q = ceil(log(acdectol)/log(rho)); % minimum lags to achieve specified tolerance
--- testing/isitthere.m
+++
@@ -1,6 +0,0 @@
-
-try
-    asdasda
-catch except
-    fprintf('It''s not there: ''%s'', ''%s''\n', except.identifier,except.message);
-end
