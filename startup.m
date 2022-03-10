789d9f8ebd20456a02c0b0fc7e49f693e1270ae3
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2022-03-10 16:06:17 +0000

    Fixed bug for residuals returned by tsdata_to_var(thanks to Daniel Titheradge for heads-up)

--- startup.m
+++ startup.m
@@ -13,13 +13,13 @@
 
 global mvgc_version;
 mvgc_version.major = 1;
-mvgc_version.minor = 2;
+mvgc_version.minor = 3;
 
-fprintf('[mvgc startup] Initialising MVGC toolbox version %d.%d\n', mvgc_version.major, mvgc_version.minor);
+fprintf('[MVGC startup] Initialising MVGC toolbox version %d.%d\n', mvgc_version.major, mvgc_version.minor);
 
 %% Set path
 
-% Add mvgc root directory and appropriate subdirectories to path
+% Add MVGC root directory and appropriate subdirectories to path
 
 global mvgc_root;
 mvgc_root = fileparts(mfilename('fullpath')); % directory containing this file
@@ -46,7 +46,7 @@ addpath(fullfile(mvgc_root,'docs')); % don't add the 'html' subdirectory
 % addpath(fullfile(mvgc_root,'testing'));
 % addpath(fullfile(mvgc_root,'maintainer'));
 
-fprintf('[mvgc startup] Added MVGC root directory %s and subdirectories to path\n',mvgc_root);
+fprintf('[MVGC startup] Added MVGC root directory %s and subdirectories to path\n',mvgc_root);
 
 %% Check |mex| files
 
@@ -55,10 +55,10 @@ fprintf('[mvgc startup] Added MVGC root directory %s and subdirectories to path\
 global have_genvar_mex;
 have_genvar_mex = exist('genvar_mex','file') == 3;
 if ~have_genvar_mex
-    fprintf(2,'[mvgc startup] WARNING: no ''genvar'' mex file found; please run the ''mvgc_makemex'' script.\n');
-    fprintf(2,'[mvgc startup]          Meanwhile, a slower scripted VAR simulation routine will be used.\n');
+    fprintf(2,'[MVGC startup] WARNING: no ''genvar'' mex file found; please run the ''mvgc_makemex'' script.\n');
+    fprintf(2,'[MVGC startup]          Meanwhile, a slower scripted VAR simulation routine will be used.\n');
 else
-    fprintf('[mvgc startup] All MVGC ''mex'' files for your platform exist\n');
+    fprintf('[MVGC startup] All MVGC ''mex'' files for your platform exist\n');
 end
 
 %% Check for dependencies on other Matlab(R) toolboxes
@@ -66,30 +66,30 @@ end
 % Check if we have Statistics toolbox - see if ch2cdf is present
 
 if fexists(@chi2cdf)
-    fprintf('[mvgc startup] Statistics Toolbox(TM) seems to be present.\n');
+    fprintf('[MVGC startup] Statistics Toolbox(TM) seems to be present.\n');
 else
 	addpath(fullfile(mvgc_root,'utils','stats'));
-    fprintf(2,'[mvgc startup] WARNING: Statistics Toolbox(TM) does not seem to be present.\n');
-    fprintf(2,'[mvgc startup]          Will use slower scripted routines (see utils/stats directory).\n');
+    fprintf(2,'[MVGC startup] WARNING: Statistics Toolbox(TM) does not seem to be present.\n');
+    fprintf(2,'[MVGC startup]          Will use slower scripted routines (see utils/stats directory).\n');
 end
 
 % Check if we have Signal Processing toolbox - see if pwelch is present
 
 if fexists(@pwelch)
-    fprintf('[mvgc startup] Signal Processing Toolbox(TM) seems to be present.\n');
+    fprintf('[MVGC startup] Signal Processing Toolbox(TM) seems to be present.\n');
 else
-    fprintf(2,'[mvgc startup] WARNING: Signal Processing Toolbox(TM) does not seem to be present.\n');
-    fprintf(2,'[mvgc startup]          Some spectral estimation routines may not work.\n');
+    fprintf(2,'[MVGC startup] WARNING: Signal Processing Toolbox(TM) does not seem to be present.\n');
+    fprintf(2,'[MVGC startup]          Some spectral estimation routines may not work.\n');
 end
 
 % Check if we have 'dlyap' from the Control System toolbox
 
 if fexists(@dlyap)
-    fprintf('[mvgc startup] Control System Toolbox(TM) seems to be present.\n');
+    fprintf('[MVGC startup] Control System Toolbox(TM) seems to be present.\n');
 else
 	addpath(fullfile(mvgc_root,'utils','control'));
-    fprintf(2,'[mvgc startup] WARNING: Control System Toolbox(TM) does not seem to be present.\n');
-    fprintf(2,'[mvgc startup]          Will use slower scripted routines (see utils/control directory).\n');
+    fprintf(2,'[MVGC startup] WARNING: Control System Toolbox(TM) does not seem to be present.\n');
+    fprintf(2,'[MVGC startup]          Will use slower scripted routines (see utils/control directory).\n');
 end
 
 %% Initialise default random number stream
@@ -100,12 +100,12 @@ end
 
 rng_seed(-1); % seed from /dev/urandom (Unix/Mac) else from clock
 
-fprintf('[mvgc startup] Random number generator initialised\n');
+fprintf('[MVGC startup] Random number generator initialised\n');
 
 %% Enable all warnings
 
 warning on all
-fprintf('[mvgc startup] All warnings enabled\n');
+fprintf('[MVGC startup] All warnings enabled\n');
 
 % Important notes to users
 
@@ -121,9 +121,9 @@ fprintf('[MVGC startup]\n');
 
 % Done
 
-fprintf('[mvgc startup] Initialisation complete (you may re-run ''startup'' at any time)\n');
+fprintf('[MVGC startup] Initialisation complete (you may re-run ''startup'' at any time)\n');
 
-fprintf('[mvgc startup] Type ''helpon'' to get started\n');
+fprintf('[MVGC startup] Type ''helpon'' to get started\n');
 
 %%
 % <startup.html back to top>
